bc7b482ebefd77911acfaff7397208f5c895dac5
Varun Master <varunmaster95@gmail.com>
2021-04-18 02:25:44 -0400

    added email option

--- dockerHeartbeat.sh
+++ dockerHeartbeat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "$(date +'%m/%d/%Y %H:%M:%S') ----------> Docker heartbeat script start <----------" >> dockerHeartbeat.log
+echo "$(date +'%m/%d/%Y %H:%M:%S') ----------> Docker heartbeat script start <----------" >> /Scripts/dockerHeartbeat.log
 dockerService="docker"
 dockerServiceStatus="$(systemctl is-active $dockerService)"
 dockerContainerName="site4000-redis"
@@ -8,11 +8,13 @@ dockerContainerStatus="$(docker container inspect $dockerContainerName | grep -P
 
 if [ $dockerServiceStatus = "active" ];
 then
-        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker service is already running" >> dockerHeartbeat.log
+        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker service is already running" >> /Scripts/dockerHeartbeat.log
 #       echo "if is true"
 else
         systemctl start $dockerService
-        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker service not running..started it now" >> dockerHeartbeat.log
+        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker service not running..started it now" >> /Scripts/dockerHeartbeat.log
+        echo "Docker service restarted now $(date +'%m/%d/%Y %H:%M:%S') on E3U20-DockerRedis" | mail -s "Docker Service restarted" v@gmail.com
+        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> sent email for docker service restart" >> /Scripts/dockerHeartbeat.log
 #       echo "if is false"
 fi
 
@@ -23,9 +25,12 @@ sleep 3
 if [ $dockerContainerStatus != "running" ];
 then
         docker start $dockerContainerName
-        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker container is not running...started it now" >> dockerHeartbeat.log
+        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker container is not running...started it now" >> /Scripts/dockerHeartbeat.log
+        echo "Docker container $dockerContainerName restarted now $(date +'%m/%d/%Y %H:%M:%S') on E3U20-DockerRedis" | mail -s "Docker container restarted" v@gmail.com
+        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> sent email for docker container restart" >> /Scripts/dockerHeartbeat.log
 else
-        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker container is already running" >> dockerHeartbeat.log
+        echo "$(date +'%m/%d/%Y %H:%M:%S') ----> Docker container is already running" >> /Scripts/dockerHeartbeat.log
 fi
 
-echo "$(date +'%m/%d/%Y %H:%M:%S') -----------> Docker heartbeat script end <----------- " >> dockerHeartbeat.log
+echo "$(date +'%m/%d/%Y %H:%M:%S') -----------> Docker heartbeat script end <----------- " >> /Scripts/dockerHeartbeat.log
+echo "##################################################################################" >> /Scripts/dockerHeartbeat.log
